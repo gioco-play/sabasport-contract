bbcf23bfe3fbf210766eadb43e6290aaf6b041cd
q90016200 <q90016200@gmail.com>
2022-01-18 16:05:02 +0800

    first commit

---
+++ src/Contract/SabaSportServiceInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+namespace GiocoPlus\SabaSport\Contract;
+
+
+interface SabaSportServiceInterface
+{
+    /**
+     * 遊戲啟動
+     *
+     * @param string $op_code
+     * @param string $account
+     * @param string $game_code
+     * @param string $language
+     * @param string $backUrl
+     * @return mixed
+     */
+    function gameLaunch(string $op_code, string $account, string $game_code, string $language);
+
+}
