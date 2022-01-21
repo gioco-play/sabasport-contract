a0007ee5e6b792d966bd44170f85527af80c663c
q90016200 <q90016200@gmail.com>
2022-01-21 16:40:52 +0800

    update

--- src/Contract/SabaSportServiceInterface.php
+++ src/Contract/SabaSportServiceInterface.php
@@ -13,7 +13,6 @@ interface SabaSportServiceInterface
      * @param string $account
      * @param string $game_code
      * @param string $language
-     * @param string $backUrl
      * @return mixed
      */
     function gameLaunch(string $op_code, string $account, string $game_code, string $language);
