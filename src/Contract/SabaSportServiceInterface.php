4b45eccaf59010358fe07d03f7e87930c7abb58f
q90016200 <q90016200@gmail.com>
2022-08-04 10:46:23 +0800

    添加帳號轉換

--- src/Contract/SabaSportServiceInterface.php
+++ src/Contract/SabaSportServiceInterface.php
@@ -18,4 +18,21 @@ interface SabaSportServiceInterface
      */
     function gameLaunch(string $op_code, string $vendor_code, string $account, string $game_code, string $language);
 
+    /**
+     * 營商帳號轉換為遊戲商帳號
+     * @param string $op_code
+     * @param array $vendor
+     * @param string $player_name
+     * @return mixed
+     */
+    function accountToVendor(string $op_code, array $vendor, string $player_name);
+
+    /**
+     * 遊戲商帳號轉換為營商帳號
+     * @param string $op_code
+     * @param array $vendor
+     * @param string $vendor_account
+     * @return mixed
+     */
+    function accountToOperator(string $op_code, array $vendor, string $vendor_account);
 }
